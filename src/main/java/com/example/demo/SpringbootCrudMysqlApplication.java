8112c4a73c74914cd8ff3c232057e1711d0793a4
Himanshu Soni <himanshusoni3105@outlook.com>
2021-06-11 12:27:14 +0530

    First commit

---
+++ src/main/java/com/example/demo/SpringbootCrudMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudMysqlApplication.class, args);
+	}
+
+}
