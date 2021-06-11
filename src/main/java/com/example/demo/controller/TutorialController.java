a9e21a1d5a591fef3060d60d1be0068f3e2f9d9b
Himanshu Soni <himanshusoni3105@outlook.com>
2021-06-11 18:34:44 +0530

    after model class completion

--- src/main/java/com/example/demo/controller/TutorialController.java
+++ src/main/java/com/example/demo/controller/TutorialController.java
@@ -1,5 +1,55 @@
 package com.example.demo.controller;
 
-public class TutorialController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Tutorial;
+import com.example.demo.repository.TutorialRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+	@Autowired
+	TutorialRepository tutorialRepository;
+	
+	@GetMapping("/tutorials")
+	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+		return null;
+	}
+	
+	@GetMapping("/tutorial/{id}")
+	public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id){
+		return null;
+	}
+	
+	@PostMapping("/tutorials")
+	public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial){
+		return null;
+	}
+	
+	@PutMapping("/tutorial/{id}")
+	public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial){
+		return null;
+	}
+	
+	@DeleteMapping("/tutorials")
+	public ResponseEntity<HttpStatus> deleteAllTutorials(){
+		return null;
+	}
+	
+	@DeleteMapping("/tutorials/{id}")
+	public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id){
+		return null;
+	}
+	
+	@GetMapping("/tutorial/published")
+	public ResponseEntity<List<Tutorial>> findByPublished(){
+		return null;
+	}
 }
--- src/main/java/com/example/demo/repository/TutorialRepository.java
+++ src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repository;
+import java.util.List;
 
-public interface TutorialRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Tutorial;
+
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
 
 }
